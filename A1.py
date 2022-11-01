b00ebb1144db7a070893c270df953aa97e0ec4ef
NatthananChaitrakulthong <117083336+NatthananChaitrakulthong@users.noreply.github.com>
2022-11-01 09:29:16 +0000

    Add files via upload

---
+++ A1.py
@@ -0,0 +1,11 @@
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import numpy as np
+hdulist= fits.open('/Users/natthanan/OneDrive - Imperial College London/Imperial College/Year 3/Practical Physics/A1_mosaic.fits')
+headers = hdulist[0].header
+data = hdulist[0].data
+data = data.flatten()
+data_filter = [d for d in data if (d>7000).all() ]
+plt.hist(data_filter, bins=200)
+plt.yscale('log')
+plt.show()
